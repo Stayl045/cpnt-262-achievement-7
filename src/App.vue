cefa025feda0ef62ae0c4389fa8c8674934cd640
Stephan Taylor <stephantaylor21@hotmail.com>
2022-03-24 13:51:08 -0600

    added template content into componenets

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <TheBaseHeader />
+  <BaseCard />
 </template>
 
 <script setup>
   import { computed } from "vue";
   import TheBaseHeader from "./components/layout/TheBaseHeader.vue";
+  import BaseCard from "./components/card/BaseCard.vue";
 </script>
---
+++ src/components/card/BaseCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <BaseCardTitle />
+  <BaseCardImage />
+  <BaseCardText />
+</template>
+
+<script setup>
+  import BaseCardTitle from "./BaseCardTitle.vue";
+  import BaseCardImage from "./BaseCardImage.vue";
+  import BaseCardText from "./BaseCardText.vue";
+</script>
